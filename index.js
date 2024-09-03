3d76710f69660e16a9df428aea66f7624f4dd089
Bhushan <bhu3203298s007@gmail.com>
2024-09-03 17:23:28 +0530

    changed

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const age = 22;
 const name = "Bhushan jadhav";
 const dob = '12/7/2022';
+const isAdult = () => {
+    if (age > 18) {
+        return true;
+    }
+    else (){
+        return false;
+    }
+}

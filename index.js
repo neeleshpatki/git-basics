820d1da91b8d6d81c49f28714460a0583a7222ca
Neelesh <neeleshpatki@gmail.com>
2024-09-03 17:25:28 +0530

    nano

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const age = 26;
 const name = 'Neelesh';
 
 const dob = '23/05/1998'
+const isAdult = () => {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
+        
+}

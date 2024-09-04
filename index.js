d620551efab480adb07bc3850e801dbd01c40dd2
Bhushan <bhu3203298s007@gmail.com>
2024-09-04 09:17:36 +0530

    a feature added again

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const name = "Bhushan jadhav";
 const dob = '12/7/2022';
 function AFeature() {
     console.log("this is new feature");
+    console.log("this is new feature A");
 }
 const isAdult = () => {
     if (age > 18) {

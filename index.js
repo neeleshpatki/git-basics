e325a4b9803c0c962737f8da03a864ed19ab7117
Bhushan <bhu3203298s007@gmail.com>
2024-09-04 09:55:53 +0530

    a new feature

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const age = 26;
 const name = 'Neelesh'; 
-const arr = [1,2,3,4,5];
-
+const arr = [1, 2, 3, 4, 5];
+function AFeature() {
+    console.log("this is new feature");
+}
 const dob = '23/05/1998'
 const isAdult = () => {
     if (age > 18) {
